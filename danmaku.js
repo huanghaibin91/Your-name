28270373a814554b01557d02d95448741aa73658
huanghaibin91 <huanghaibin91@126.com>
2017-02-25 19:07:59 +0800

    Revert "2/25"
    This reverts commit 11df77394be63b6c811f8b364d1f8290fcaf0847.

--- danmaku.js
+++ danmaku.js
@@ -1,24 +1,11 @@
 window.onload = function() {
     var movie = document.getElementById('movie');
     var playerBox = document.querySelector('.player-box');
-    var progressBox = document.querySelector('#progress-box');
     playerBox.addEventListener('click', function() {
         play();
     }, false);
-    playerBox.addEventListener('mouseover', function() {
-        progressBox.className = 'active';
-    }, false);
-    playerBox.addEventListener('mouseout', function() {
-        progressBox.className = '';
-    }, false);
 }
-function toggleClassName(ele, cls) {
-    if (ele.className == cls) {
-        ele.className = '';
-    } else {
-        ele.className = cls;
-    }
-}
+
 function play() {
     var playcontrol = document.getElementById('play-control');
     if (movie.paused || movie.ended) {

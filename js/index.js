68a13e85b341371a69f329c755d1ba22d4951c48
huanghaibin91 <huanghaibin91@126.com>
2017-03-18 20:21:54 +0800

    3/18
    3/18 commit

--- js/index.js
+++ js/index.js
@@ -1,103 +1,121 @@
 window.onload = function() {
-    var movie = document.getElementById('movie');
-    var playerBox = document.querySelector('.player-box');
-    var progressBox = document.querySelector('#progress-box');
-    // 元素居中
-    var productionList = getClass('production-list');
-    var close = getClass('close');
-    // for (var i = 0; i < productionList.length; i++) {
-    //     productionList[i].style.top = (getInner().height - productionList[i].offsetHeight) / 2 + 'px';
-    // }
+    (function() {
+        var movie = document.getElementById('movie');
+        var playerBox = getClass('player-box')[0];
+        var playcontrol = document.getElementById('play-control');
+        var progressBox = document.getElementById('progress-box');
+        var progress = document.getElementById('progress');
+        var fullScreen = getClass('fullscreen')[0];
+        // 播放暂停切换
+        playerBox.addEventListener('click', function(e) {
+            if (e.target.id != 'progress-box' && e.target.id != 'progress') {
+                play();
+            }
+        }, false);
+        // 进度条控制
+        playerBox.addEventListener('mouseover', function() {
+            addClass(progressBox, 'active');
+        }, false);
+        playerBox.addEventListener('mouseout', function() {
+            removeClass(progressBox, 'active');
+        }, false);
+        movie.addEventListener('timeupdate', function() {
+            var duration = movie.duration;
+            var currTime = movie.currentTime;
+            var pre = currTime / duration * 100 + '%';
+            progress.style.width = pre;
+            if (duration === currTime) {
+                movie.currentTime = 0;
+                removeClass(playcontrol, 'hide');
+            }
+        }, false);
+        // 进度条跳转
+        progressBox.addEventListener('click', function(e) {
+            console.log(e.offsetX);
+            movie.currentTime = (e.offsetX / this.offsetWidth) * movie.duration;
+        }, false);
+        // 全屏
+        fullScreen.addEventListener('click', function() {
+            if (movie.requestFullscreen) {
+                movie.requestFullscreen();
+            } else if (movie.mozRequestFullScreen) {
+                movie.mozRequestFullScreen();
+            } else if (movie.webkitRequestFullScreen) {
+                movie.webkitRequestFullScreen();
+            }
+        }, false);
+        // 播放暂停切换
+        function play() {
+            if (movie.paused || movie.ended) {
+                if (movie.ended) {
+                    movie.currentTime = 0;
+                }
+                movie.play();
+            } else {
+                movie.pause();
+            }
+            toggleClassName(playcontrol, 'hide');
+        }
+    }());
+    // 立即函数
+    (function() {
+        // 点击页面滚动到指定位置
+        var nav = getClass('nav')[0];
+        nav.addEventListener('click', function(e) {
+            var target = e.target;
+            if (target.nodeName == 'LI') {
+                switch (target.id) {
+                    case 'home-page':
+                        scrollMove(0);
+                        break;
+                    case 'film-synopsis':
+                        scrollMove(610);
+                        break;
+                    case 'character-introduction':
+                        scrollMove(1250);
+                        break;
+                    case 'production-personnel':
+                        scrollMove(3100);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }, false);
+        function scrollMove(target) {
+            var timer = setInterval(function() {
+                var nowTop = document.documentElement.scrollTop || document.body.scrollTop;
+                var speed = (target - nowTop) / 10;
+                speed = speed > 0
+                    ? Math.ceil(speed)
+                    : Math.floor(speed);
+                document.documentElement.scrollTop = document.body.scrollTop = nowTop + speed;
+                if (nowTop == target) {
+                    clearInterval(timer);
+                }
+            }, 30);
+        }
+    }());
     // 切换显示作品列表
-    var production = getClass('production');
-    for (var i = 0; i < production.length; i++) {
-        (function(i) {
-            production[i].onclick = function() {
-                // for (var j = 0; j < productionList.length; j++) {
-                //     addClass(productionList[i], 'hide');
-                // }
-                removeClass(productionList[i], 'hide');
-                // document.body.style.overflow = 'hidden';
-                productionList[i].style.top = (getInner().height - productionList[i].offsetHeight) / 2 + 'px';
-            }
-            close[i].onclick = function() {
-                addClass(productionList[i], 'hide');
-            }
-        })(i);
-    }
-
-    playerBox.addEventListener('click', function() {
-        play();
-    }, false);
-    playerBox.addEventListener('mouseover', function() {
-        progressBox.className = 'active';
-    }, false);
-    playerBox.addEventListener('mouseout', function() {
-        progressBox.className = '';
-    }, false);
-}
-// 播放暂停切换
-function play() {
-    var playcontrol = document.getElementById('play-control');
-    if (movie.paused || movie.ended) {
-        if (movie.ended) {
-            movie.currentTime = 0;
+    (function() {
+        var production = getClass('production');
+        var productionList = getClass('production-list');
+        var close = getClass('close');
+        for (var i = 0; i < production.length; i++) {
+            (function(i) {
+                production[i].addEventListener('click', function() {
+                    removeClass(productionList[i], 'hide');
+                    productionList[i].style.top = (getInner().height - productionList[i].offsetHeight) / 2 + 'px';
+                }, false);
+                close[i].addEventListener('click', function() {
+                    addClass(productionList[i], 'hide');
+                }, false);
+            })(i);
         }
-        movie.play();
-        playcontrol.className = 'hidden';
-    } else {
-        movie.pause();
-        playcontrol.className = 'show';
-    }
+    }());
 }
 
-function getProgress() {
-    var movie = document.getElementById('movie');
-    var progressBox = document.querySelector('.progress-box');
-    var progress = document.querySelector('.progress');
-    var percent = movie.currentTime / movie.duration;
-    progress.style.width = percent * (progressBox.offsetWidth) + "px";
-}
-
-// 立即函数
-(function() {
-    // 点击页面滚动到指定位置
-    var nav = getClass('nav')[0];
-    nav.addEventListener('click', function(e) {
-        var target = e.target;
-        if (target.nodeName == 'LI') {
-            switch (target.id) {
-                case 'home-page':
-                    scrollMove(0);
-                    break;
-                case 'film-synopsis':
-                    scrollMove(610);
-                    break;
-                case 'character-introduction':
-                    scrollMove(1250);
-                    break;
-                case 'production-personnel':
-                    scrollMove(3100);
-                    break;
-                default:
-                    break;
-            }
-        }
-    }, false);
-    function scrollMove(target) {
-        var timer = setInterval(function() {
-            var nowTop = document.documentElement.scrollTop || document.body.scrollTop;
-            var speed = (target - nowTop) / 10;
-            speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
-            document.documentElement.scrollTop = document.body.scrollTop = nowTop + speed;
-            if (nowTop == target) {
-                clearInterval(timer);
-            }
-        }, 30);
-    }
-})();
-
-
+// 常用的一些方法
 // 获取类
 function getClass(cls, id) {
     var node = null;
@@ -140,7 +158,7 @@ function toggleClassName(ele, cls) {
     if (!hasClass(ele, cls)) {
         ele.className += ' ' + cls;
     } else {
-        ele.className = replace(new RegExp('(\\s|^)' + cls + '\\s|$'), ' ');
+        ele.className = ele.className.replace(new RegExp('(\\s|^)' + cls + '(\\s|$)'), ' ');
     }
 }
 // 获取窗口宽高
